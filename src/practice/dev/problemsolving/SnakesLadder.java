db651aa9cd1f09c82ea11eddb96cec0022e29bfa
devndone <devndone@gmail.com>
2018-06-21 16:31:19 +0530

    Corrected the compile issues and added snackladder game (from geeksforgeeks)

--- src/practice/dev/problemsolving/SnakesLadder.java
+++ src/practice/dev/problemsolving/SnakesLadder.java
@@ -1,5 +1,16 @@
 package practice.dev.problemsolving;
 
+/*
+
+https://www.geeksforgeeks.org/snake-ladder-problem-2/
+
+Snake and Ladder Problem:
+Given a snake and ladder board, find the minimum number of dice throws required to reach the destination or last cell from source or 1st cell. Basically, the player has total control over outcome of dice throw and wants to find out minimum number of throws required to reach last cell.
+
+If the player reaches a cell which is base of a ladder, the player has to climb up that ladder and if reaches a cell is mouth of the snake, has to go down to the tail of snake without a dice throw.
+
+*/
+
 // Java program to find minimum number of dice
 // throws required to reach last cell from first
 // cell of a given snake and ladder board
